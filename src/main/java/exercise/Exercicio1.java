9deab519c5e31f3e9a644a1f7098bb9f728c40a2
crisfischer <cristianealinefischer@gmail.com>
2017-07-26 11:38:49 -0300

    Exercicios1

--- src/main/java/exercise/Exercicio1.java
+++ src/main/java/exercise/Exercicio1.java
@@ -1,11 +1,18 @@
 package exercise;
 
+import java.util.List;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
+import core.WebDriverManager;
 import core.WebDriverRunner;
+import steps.WikiStep;
 
 /**
  * A partir da página https://en.wikipedia.org/wiki/Main_Page, imprimir o nome das pessoas que morreram recentemente.
@@ -19,6 +26,23 @@ public class Exercicio1 {
 
 	@Test
 	public void run() {
+		
+		WebDriver webdriver = WebDriverManager.getWebDriver();
+		webdriver.get("https://en.wikipedia.org/wiki/Main_Page");
+		By byInputSearch = By.xpath("//div/b/a[text()='Recent deaths']/../..//li");
+		List<WebElement> findElements = webdriver.findElements(byInputSearch);
+		
+		for(WebElement we:findElements) {
+			
+			String x=we.getText();
+			
+			System.out.println(x);
+			
+	
+		}
+			
+			
+		
 		// TODO Auto-generated method stub
 
 	}
--- src/main/java/exercise/Exercicio2.java
+++ src/main/java/exercise/Exercicio2.java
@@ -1,9 +1,16 @@
 package exercise;
 
+import java.util.List;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import core.WebDriverManager;
 import core.WebDriverRunner;
 
 /**
@@ -23,6 +30,20 @@ public class Exercicio2 {
 	@Test
 	public void run() {
 		// TODO Auto-generated method stub
+		
+		WebDriver webdriver = WebDriverManager.getWebDriver();
+		webdriver.get("https://en.wikipedia.org/wiki/Main_Page");
+		By xpath = By.xpath("//div[@id='p-lang']//li/a");
+        List<WebElement> findElements = webdriver.findElements(xpath);
+		
+		for(WebElement we:findElements) {
+			
+		String x =we.getAttribute("title");
+		
+		System.out.println(x);
+		
+		}
+		
 
 	}
 
