56abf71cde032fd6831ea516f3d86656eefd5496
otavioribeiro <30343059+otavioribeiro@users.noreply.github.com>
2017-07-26 11:49:28 -0300

    Exercicios1,2,3

--- src/main/java/exercise/Exercicio2.java
+++ src/main/java/exercise/Exercicio2.java
@@ -4,7 +4,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import core.WebDriverManager;
 import core.WebDriverRunner;
+import pageobject.WikiPag;
 
 /**
  * A partir da página https://en.wikipedia.org/wiki/Main_Page,
@@ -23,7 +29,19 @@ public class Exercicio2 {
 	@Test
 	public void run() {
 		// TODO Auto-generated method stub
-
+		WebDriver webdriver = WebDriverManager.getWebDriver();
+		webdriver.get(WikiPag.url);
+		
+		//pesquisa pelo termo
+		By byInputSearch = By.id("searchInput");
+		WebElement elementInputSearch = webdriver.findElement(byInputSearch);
+		elementInputSearch.sendKeys(termo);
+		webdriver.findElement(By.id("searchButton")).click();
+		
+		String xpath = "(.//div[@id='p-lang']/div/ul)";
+		System.out.println(webdriver.findElement(By.xpath(xpath)).getText());
+		
+		
 	}
 
 }
