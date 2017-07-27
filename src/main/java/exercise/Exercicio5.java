ea67aa4541c5681205a171494eb87660248eacdc
fabioreibnitz <fsreibnitz@hotmail.com>
2017-07-27 18:41:48 -0300

    Alteracoes, colocando funcao mudarTela

--- src/main/java/exercise/Exercicio5.java
+++ src/main/java/exercise/Exercicio5.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.server.handler.FindElements;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.w3c.dom.Text;
 
 import core.ParameterizedTest;
 import core.WebDriverManager;
@@ -36,6 +37,8 @@ public class Exercicio5 extends ParameterizedTest {
 	private String cpf;
 	private String senha;
 
+	
+	
 	public Exercicio5 (String cpf, String senha) {
 		this.cpf = cpf;
 		this.senha = senha;
@@ -54,25 +57,69 @@ public class Exercicio5 extends ParameterizedTest {
 		WebElement acessar_password = webdriver.findElement(password);
 
 		
-		acessar_password.sendKeys(senha);		
-		By botao = By.xpath("//button");
-		WebElement click_botao = webdriver.findElement(botao);
-		click_botao.click();
+		acessar_password.sendKeys(senha);	
+//		fazer Login
+		mudarTela(webdriver,  "//button"); 
+//		Se houver mais de um perfil, escolher Digitador
+		mudarTela(webdriver,"//div[contains(text(),'DIGITADOR')]"); 
+//		Acessar Modulo CDS
+		mudarTela(webdriver, "//div[@peid='class br.gov.saude.esus.EsusUserMenuViewImpl.cds']"); 
+//		Acessar Modulo cadastro individual
+		mudarTela(webdriver, ("//div[@style='background-image: url(\"images/principal/cadastro_individual.png\"); "
+											+ "user-select: none; position: absolute; z-index: 10; background-position: 0px 0px; "
+											+ "width: 225px; height: 94px; left: 0px; top: 0px;']"));
+;
+		
+//		Acessar Adicionar Ficha
+		mudarTela(webdriver, "//div[@peid='FichaCadastroIndividualListingViewImpl.Adicionar']");
+//		Cancelar sem preencher nenhum campo
+		mudarTela(webdriver,"//div[@peid='CadastroIndividualDetailViewImpl.Cancelar']");
+		
+		
+		
+//		Essas linhas tinha feito quando estava Bridge, 
+//		By botao = By.xpath("//button");
+//		WebElement click_botao = webdriver.findElement(botao);
+//		click_botao.click();
 
-		By digitador = By.xpath("//div[contains(text(),'DIGITADOR')]");
-		WebDriverWait wait = new WebDriverWait(webdriver, 5);
-
-		WebElement label = wait.until(ExpectedConditions.visibilityOfElementLocated(digitador));
+//		By digitador = By.xpath("//div[contains(text(),'DIGITADOR')]");
+//		WebDriverWait wait = new WebDriverWait(webdriver, 5);
+//
+//		WebElement label = wait.until(ExpectedConditions.visibilityOfElementLocated(digitador));
 		//WebElement acessar_digitador = webdriver.findElement(digitador);
 
-		label.click();
+//		label.click();
 		
-		By cds = By.xpath("//div[@peid='class br.gov.saude.esus.EsusUserMenuViewImpl.cds']");
-		WebDriverWait wait2 = new WebDriverWait(webdriver, 5);
+//		By cds = By.xpath("//div[@peid='class br.gov.saude.esus.EsusUserMenuViewImpl.cds']");
+//
+//		WebElement painel = wait.until(ExpectedConditions.visibilityOfElementLocated(cds));
+//		
+//		painel.click();
+//		
+//		By cadastro_individual = By.xpath(("//div[@style='background-image: url(\"images/principal/cadastro_individual.png\"); "
+//											+ "user-select: none; position: absolute; z-index: 10; background-position: 0px 0px; "
+//											+ "width: 225px; height: 94px; left: 0px; top: 0px;']"));
+//	
+//		    WebElement painel_cadastro = wait.until(ExpectedConditions.visibilityOfElementLocated(cadastro_individual));
+//			painel_cadastro.click();
+//			cadastro_individual= cadastro_individual.xpath("//div[@peid='FichaCadastroIndividualListingViewImpl.Adicionar']");
+//			WebElement adicao = wait.until(ExpectedConditions.visibilityOfElementLocated(cadastro_individual));
+//			adicao.click();
+//			cadastro_individual= cadastro_individual.xpath("//div[@peid='CadastroIndividualDetailViewImpl.Cancelar']");
+//			adicao = wait.until(ExpectedConditions.visibilityOfElementLocated(cadastro_individual));
+//			adicao.click();
 
-		WebElement painel = wait2.until(ExpectedConditions.visibilityOfElementLocated(cds));
-		
+	}
+	
+	public static void mudarTela(WebDriver webdriver,String xpath) {
+		WebDriverWait wait = new WebDriverWait(webdriver, 5);
+		By botao = By.xpath(xpath);
+		//WebElement click_botao = webdriver.findElement(botao);
+		WebElement painel = wait.until(ExpectedConditions.visibilityOfElementLocated(botao));
 		painel.click();
+
+		
+
 	}
 	
 		@Parameters
