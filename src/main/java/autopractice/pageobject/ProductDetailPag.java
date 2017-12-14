673ec1880aab12601b97a107131eb3318e093000
evandrocoan <evandrocoan@hotmail.com>
2017-12-14 15:13:00 -0200

    Fixed ProductDetailPag.java formatting.

--- src/main/java/autopractice/pageobject/ProductDetailPag.java
+++ src/main/java/autopractice/pageobject/ProductDetailPag.java
@@ -8,32 +8,33 @@ import core.WebDriverManager;
 
 public class ProductDetailPag {
 
-    public static WebDriver driver = WebDriverManager.getWebDriver();
-    public static String xpathProductDetail = "TODO";
-    public static String xpathProductContainer = "//div[@class='product-container']";
+	public static WebDriver driver = WebDriverManager.getWebDriver();
+	public static String xpathProductDetail = "TODO";
+	public static String xpathProductContainer = "//div[@class='product-container']";
 
-    public static By by() {
-	return By.xpath(xpathProductDetail);
-    }
+	public static By by() {
+		return By.xpath(xpathProductDetail);
+	}
 
-    public static By byPlusItemButton() {
-	return By.xpath("//*[@class='icon-plus']");
-    }
+	public static By byPlusItemButton() {
+		return By.xpath("//*[@class='icon-plus']");
+	}
 
-    public static void dropdownSelectElement(String visibleText) {
-	Select dropdown = new Select(driver.findElement(By.xpath("//*[@class='form-control attribute_select no-print']")));
-	dropdown.selectByVisibleText(visibleText);
-    }
-    
-    public static By byGreenColorButton() {
-	return By.xpath("//*[@name='Green' and contains(@class, 'color_pick')]");
-    }
-    
-    public static By byAddToCartButton() {
-	return By.xpath("//*[text()='Add to cart']");
-    }
+	public static void dropdownSelectElement(String visibleText) {
+		Select dropdown = new Select(
+				driver.findElement(By.xpath("//*[@class='form-control attribute_select no-print']")));
+		dropdown.selectByVisibleText(visibleText);
+	}
 
-    public static By byTotalPurchasePriceButton() {
-	return By.xpath("//*[@class='ajax_block_cart_total']");
-    }
+	public static By byGreenColorButton() {
+		return By.xpath("//*[@name='Green' and contains(@class, 'color_pick')]");
+	}
+
+	public static By byAddToCartButton() {
+		return By.xpath("//*[text()='Add to cart']");
+	}
+
+	public static By byTotalPurchasePriceButton() {
+		return By.xpath("//*[@class='ajax_block_cart_total']");
+	}
 }
